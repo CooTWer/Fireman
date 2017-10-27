0ea683f06790b1f0d947222c1e9c2a99602227aa
rhuanhuan <1210098674@qq.com>
2017-10-27 23:40:27 +0800

    [Huan] Make app display a detail page

--- fireman/FiremanMobile/index.js
+++ fireman/FiremanMobile/index.js
@@ -2,7 +2,6 @@ import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import CompanyList from './src/CompanyList';
 import CarouselSnap from './src/Components/CarouselComponent/CarouselSnap';
-// import CompanyDetail from './src/CompanyDetail';
 
 export const SimpleApp = StackNavigator({
     Home: {screen: CompanyList},
--- fireman/FiremanMobile/src/CompanyDetail.js
+++ fireman/FiremanMobile/src/CompanyDetail.js
@@ -1,14 +1,87 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {ScrollView, View, Text, Image, TextInput, StyleSheet} from 'react-native';
+import { Icon } from 'react-native-elements'
+import CompanyList from './CompanyList';
 
-export default class CompanyDetail extends React.Component{
-    render(){
-        return(
-            <View>
-                <Text style={{width: 50, height:50, background:'powderblue'}}>第一排</Text>
-                <Text style={{width: 50, height: 50, backgroundColor: 'skyblue'}} >第二排</Text>
-                <Text style={{width: 50, height: 50, backgroundColor: 'steelblue'}} >第三排</Text>
-            </View>
+export default class CompanyDetail extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            companyList: CompanyList,
+            companyDetail: companyDetail,
+            text: null,
+        };
+    }
+
+    render() {
+        return (
+            <ScrollView style={styles.base}>
+                <Image
+                    source={{uri: "/Users/rhuan/Downloads/83I58PICBzd.jpg"}}
+                    style={{width: 320, height: 220}}/>
+                {/*<TextInput*/}
+                    {/*style={{height: 40, borderColor: 'gray', borderWidth: 1}}*/}
+                    {/*onChangeText={(text) => this.setState({text})}*/}
+                    {/*placeholder={"Where do you want to go?...."}*/}
+                    {/*value={this.state.text}/>*/}
+                <View style={styles.titleName}>
+                    <Text style={styles.locationName}>通州消防局（地名）</Text>
+                    <Text style={styles.cellPhone}>电话: 010-88888888</Text>
+                </View>
+
+                <View style={styles.iconBar}>
+                    <View>
+                        <Icon name='description' />
+                        <Text style={{color: '#333333'}}>详情</Text>
+                    </View>
+
+                    <View>
+                        <Icon name='build' />
+                        <Text style={{color: '#333333'}}>设施</Text>
+                    </View>
+
+                    <View>
+                        <Icon name='people' />
+                        <Text style={{color: '#333333'}}>责任人</Text>
+                    </View>
+                </View>
+
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    base: {
+        // backgroundColor: '#eeeeee',
+    },
+    iconBar: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 5,
+        paddingTop: 5,
+        paddingLeft: 40,
+        paddingBottom: 10,
+        paddingRight: 40,
+        borderTopColor: '#eeeeee',
+        borderTopWidth: 2,
+        // backgroundColor: '#ffffff',
+    },
+    titleName: {
+        paddingTop: 15,
+        paddingLeft: 20,
+        paddingBottom: 10,
+    },
+    locationName: {
+        fontSize: 20,
+    },
+    cellPhone:{
+        marginTop: 2,
+        fontSize: 10,
+    },
+    bottom: {
+        marginBottom:10,
+    },
+});
